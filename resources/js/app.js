8ecb536012d6b3c37bdcc2d86ef579c37f058bf0
luv1111 <81750127+luv1111@users.noreply.github.com>
2021-07-03 10:58:47 +0530

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ let addToCart = document.querySelectorAll('.add-to-cart')
 
 addToCart.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        let Store1 = btn.CDATA_SECTION_NODE.Store1
+        let Store1 = btn.dataset.Store1
         console.log(Store1)
 
 
